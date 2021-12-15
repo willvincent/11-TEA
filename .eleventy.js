79b96b342231c0e6bdf5037e2c2026a4994bc0d6
Will Vincent <will@willvincent.com>
2021-12-14 22:48:46 -0600

    Fleshing out placeholder content.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,26 @@
-module.exports = () => {
+const yaml = require("js-yaml")
+const htmlmin = require("html-minifier")
+
+module.exports = (eleventyConfig) => {
+    // Disable automatic use of your .gitignore
+    eleventyConfig.setUseGitIgnore(false)
+    // Merge data instead of overriding
+    eleventyConfig.setDataDeepMerge(true)
+
+    // Support .yaml Extension in _data
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents))
+
+    // Minify HTML
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (!outputPath.endsWith(".html")) return content
+        
+        return htmlmin.minify(content, {
+            useShortDoctype: true,
+            removeComments: true,
+            collapseWhitespace: true
+        })
+    })
+
     return {
         htmlTemplateEngine: 'njk',
         dir: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './public/**/*.html',
-    './src/**/*.{md,html,vue}'
+    './src/**/*.{md,html,njk}'
   ],
   theme: {
     extend: {},
